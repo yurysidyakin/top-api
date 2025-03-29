3557147408b9f432a825f515246c6e178d5f73f6
Yury Sidyakin <yurafikys@gmail.com>
2025-03-29 16:04:59 +0300

    feat: Added TelegramModule
    Additional sending of reviews to telegram

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { getJWTConfig } from './configs/jwt.config';
 import { getMongoConfig } from './configs/mongo.config';
+import { getTelegramConfig } from './configs/telegram.config';
 import { FilesModule } from './files/files.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { SitemapModule } from './sitemap/sitemap.module';
+import { TelegramModule } from './telegram/telegram.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { UsersModule } from './users/users.module';
 
@@ -35,6 +37,11 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     FilesModule,
     SitemapModule,
+    TelegramModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getTelegramConfig,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
